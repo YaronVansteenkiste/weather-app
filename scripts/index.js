3c8e8fefaf01f03c57899e2cd9dc7de549103e43
yaronvsk <yaronvsk@googlemail.com>
2023-05-05 19:10:52 +0200

    05/05 : +Levenshtein distance to text suggestions

--- scripts/index.js
+++ scripts/index.js
@@ -75,3 +75,71 @@ searchBar.addEventListener("keypress", (event) => {
   }
   }
 });
+
+
+const searchInput = document.querySelector("#search-bar");
+const suggestionList = document.getElementById('suggestionList');
+
+const cities = [
+  'Antwerp',
+  'Mechelen',
+  'Ghent',
+  'Bruges',
+  'Liège',
+  'Namur',
+  'Amsterdam',
+  'Paris',
+  'London',
+  'Berlin',
+  'New York',
+  'Rome'
+];
+
+function levenshteinDistance(a, b) {
+  if (a.length === 0) return b.length;
+  if (b.length === 0) return a.length;
+  const matrix = Array.from(Array(b.length + 1), () => Array(a.length + 1).fill(0));
+  for (let i = 0; i <= a.length; i++) {
+    matrix[0][i] = i;
+  }
+  for (let j = 0; j <= b.length; j++) {
+    matrix[j][0] = j;
+  }
+  for (let j = 1; j <= b.length; j++) {
+    for (let i = 1; i <= a.length; i++) {
+      if (a[i - 1] === b[j - 1]) {
+        matrix[j][i] = matrix[j - 1][i - 1];
+      } else {
+        matrix[j][i] = Math.min(
+          matrix[j - 1][i] + 1,
+          matrix[j][i - 1] + 1,
+          matrix[j - 1][i - 1] + 1
+        );
+      }
+    }
+  }
+  return matrix[b.length][a.length];
+}
+
+searchInput.addEventListener('keyup', () => {
+  const searchText = searchInput.value;
+  if (searchText.length > 1) { 
+    suggestionList.innerHTML = ''; 
+    const matches = cities.filter(city => levenshteinDistance(searchText.toLowerCase(), city.toLowerCase()) < 5); 
+    matches.forEach(city => {
+      const suggestionItem = document.createElement('div');
+      suggestionItem.textContent = city;
+      suggestionItem.classList.add('suggestion-item');
+      suggestionItem.addEventListener('click', () => {
+        
+        searchInput.value = city;
+        suggestionList.innerHTML = '';
+        searchInput.dispatchEvent(new Event('search'));
+      });
+      suggestionList.appendChild(suggestionItem);
+    });
+  } else {
+    suggestionList.innerHTML = ''; 
+  }
+});
+
